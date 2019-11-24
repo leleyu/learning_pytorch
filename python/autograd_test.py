9fbf6dce140178a7ff4476fa899a187c93710c35
leleyu <yulele214@gmail.com>
2019-11-24 23:10:08 +0800

    learn autograd

---
+++ python/autograd_test.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+import torch
+
+w = torch.tensor([0.1,0.2,0.3,0.4,0.5]).float()
+x = torch.tensor([1,2,3,4,5]).float()
+w.requires_grad = True
+x.requires_grad = True
+
+wx = torch.mm(w.view(1, 5), x.view(5, 1)).view(1)
+wx.retain_grad()
+p = torch.sigmoid(wx)
+y = torch.ones(1)
+loss_fn = torch.nn.BCELoss()
+loss = loss_fn(p, y)
+loss.backward()
+print(w.grad)
+print(x.grad)
+
+
+w = torch.tensor([0.1,0.2,0.3]).float()
+x = torch.tensor([1,2,3]).float()
+w.requires_grad = True
+x.requires_grad = True
+wx = torch.mm(w.view(1,3), x.view(3, 1)).view(1) + torch.tensor([4.1])
+p = torch.sigmoid(wx)
+y = torch.ones(1)
+loss_fn = torch.nn.BCELoss()
+loss = loss_fn(p, y)
+loss.backward()
+print(w.grad)
